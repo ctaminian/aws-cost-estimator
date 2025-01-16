2a476ebd3d9bf2407c936d27d5113eca7a921e9f
ctaminian <ctaminian@gmail.com>
2025-01-15 18:37:12 -0800

    Add a simple pricing dictionary to test the code first

---
+++ pricing.py
@@ -0,0 +1,17 @@
+PRICING = {
+    "EC2": {
+        "t2.micro": 0.0116,   # USD per hour
+        "t3.micro": 0.0104,   # USD per hour
+        "t3a.micro": 0.0094,  # USD per hour
+        "m5.large": 0.096,    # USD per hour
+        "m5a.large": 0.086,   # USD per hour
+        "c5.large": 0.085,    # USD per hour
+        "c5a.large": 0.077,   # USD per hour
+        "r5.large": 0.126,    # USD per hour
+        "r5a.large": 0.113,   # USD per hour
+    },
+    "S3": {
+        "storage_per_gb": 0.023,           # USD per GB
+        "data_transfer_out_per_gb": 0.09,  # USD per GB
+    },
+}
