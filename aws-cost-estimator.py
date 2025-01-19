a90d7961babb45763dc072e736a1ee05ae117870
ctaminian <ctaminian@gmail.com>
2025-01-19 13:04:27 -0800

    Add function to exit the program with a confirmation prompt.

--- aws-cost-estimator.py
+++ aws-cost-estimator.py
@@ -6,34 +6,39 @@ def main():
         load_menu()
 
 def load_menu():
-    while True:
-        print("---------------------------------------------------")
-        print("---------------AWS Price Estimator-----------------")
-        print("---------------------------------------------------")
-        print("Which service would you like to estimate costs for?")
-        print("1. EC2")
-        print("2. S3")
-        print("3. Exit Program")
+    print("---------------------------------------------------")
+    print("---------------AWS Price Estimator-----------------")
+    print("---------------------------------------------------")
+    print("Which service would you like to estimate costs for?")
+    print("1. EC2")
+    print("2. S3")
+    print("3. Exit Program")
 
-        try:
-            choice = int(input("Please enter your choice (e.g., 1 for EC2): "))
-        except ValueError:
-            print("Invalid input! Please enter a number (e.g., 2 for S3).")
-            continue
+    try:
+        choice = int(input("Please enter your choice (e.g., 1 for EC2): "))
+    except ValueError:
+        print("Invalid input! Please enter a number (e.g., 2 for S3).")
+        return
 
-        match choice:
-            case 1:
-                estimate_ec2()
-                break
-            case 2:
-                estimate_s3()
-                break
-            case 3:
-                print("\nExiting program.")
-                print("Thank you for using the AWS price estimator.\n")
-                sys.exit()
-            case _:
-                print("Invalid choice! Please select a valid option.")
+    match choice:
+        case 1:
+            estimate_ec2()
+        case 2:
+            estimate_s3()
+        case 3:
+            confirm_exit()
+        case _:
+            print("Invalid choice! Please select a valid option.")
+
+def confirm_exit():
+    confirm = input("Are you sure you want to exit? (y/n): ").strip().lower()
+    
+    if confirm == "y":
+        print("\nExiting program.")
+        print("Thank you for using the AWS price estimator.\n")
+        sys.exit()
+    else:
+        print("\nReturning to the main menu...\n")
 
 def get_positive_int(prompt):
     while True:
